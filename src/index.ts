ce134c9cb790eb46199fefdd5377bb2839529edb
Erick <linarespachecoerick@gmail.com>
2022-11-24 22:44:27 -0500

    Initialize express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World from API');
+});
+
+app.listen(port, () =>
+  console.log(`App listening on port: ${port} \nVisit http://localhost:${port}`)
+);
